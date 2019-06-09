c00c70b7dacf3ccfc8574d282bac5100e1ca2db4
Michael <michael_0877@163.com>
2019-06-09 21:21:14 +0800

    修改公共类型

--- ktf-common/src/main/java/com/kivi/framework/dto/JwtUserDTO.java
+++ ktf-common/src/main/java/com/kivi/framework/dto/JwtUserDTO.java
@@ -6,29 +6,31 @@ import com.kivi.framework.constant.enums.UserType;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
+@NoArgsConstructor
 public class JwtUserDTO implements Serializable {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-    // 用户iD
-    @Builder.Default
-    protected Long            id               = 0L;
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+	// 用户iD
+	@Builder.Default
+	protected Long				id					= 0L;
 
-    // 用户标识
-    @Builder.Default
-    protected String          identifier       = "";
+	// 用户标识
+	@Builder.Default
+	protected String			identifier			= "";
 
-    // 用户姓名
-    @Builder.Default
-    protected String          name             = "---";
+	// 用户姓名
+	@Builder.Default
+	protected String			name				= "---";
 
-    // 用户类型
-    protected UserType        userType;
+	// 用户类型
+	protected UserType			userType;
 
 }
