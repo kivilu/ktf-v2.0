ec02182fd2336308373d4cd6329352ae94a3c92a
xueqiang <xueqiang@163.com>
2021-03-09 21:45:58 +0800

    修改SysDicDubboServiceImpl缺少的实现

--- ktf-dashboard-dubbo/src/main/java/com/kivi/dashboard/sys/dubbo/service/impl/SysDicDubboServiceImpl.java
+++ ktf-dashboard-dubbo/src/main/java/com/kivi/dashboard/sys/dubbo/service/impl/SysDicDubboServiceImpl.java
@@ -27,70 +27,75 @@ import com.kivi.framework.vo.page.PageInfoVO;
 @DubboService(version = KtfDashboardProperties.DUBBO_VERSION)
 public class SysDicDubboServiceImpl extends ServiceImpl<SysDicMapper, SysDic> implements SysDicService {
 
-	@Autowired
-	private SysDicService sysDicService;
+    @Autowired
+    private SysDicService sysDicService;
 
-	/**
-	 * 新增数据字典
-	 */
-	@KtfTrace("新增数据字典")
-	@Override
-	public Boolean save(SysDicDTO sysDicDTO) {
-		return sysDicService.save(sysDicDTO);
-	}
+    /**
+     * 新增数据字典
+     */
+    @KtfTrace("新增数据字典")
+    @Override
+    public Boolean save(SysDicDTO sysDicDTO) {
+        return sysDicService.save(sysDicDTO);
+    }
 
-	/**
-	 * 修改
-	 */
-	@KtfTrace("修改数据字典")
-	@Override
-	public Boolean updateById(SysDicDTO sysDicDTO) {
-		return sysDicService.updateById(sysDicDTO);
-	}
+    /**
+     * 修改
+     */
+    @KtfTrace("修改数据字典")
+    @Override
+    public Boolean updateById(SysDicDTO sysDicDTO) {
+        return sysDicService.updateById(sysDicDTO);
+    }
 
-	@Override
-	public SysDicDTO getDto(Long id) {
-		return sysDicService.getDto(id);
-	}
+    @Override
+    public SysDicDTO getDto(Long id) {
+        return sysDicService.getDto(id);
+    }
 
-	@Override
-	public SysDicDTO getDto(String varCode, String pVarCode) {
-		return sysDicService.getDto(varCode, pVarCode);
-	}
+    @Override
+    public SysDicDTO getDto(String varCode, String pVarCode) {
+        return sysDicService.getDto(varCode, pVarCode);
+    }
 
-	@Override
-	public List<Object> getValues(String varCode, String... pVarCodes) {
-		return sysDicService.getValues(varCode, pVarCodes);
-	}
+    @Override
+    public List<Object> getValues(String varCode, String... pVarCodes) {
+        return sysDicService.getValues(varCode, pVarCodes);
+    }
 
-	@Override
-	public Map<String, Object> getValuesMap(String varCode, String... pVarCodes) {
-		return sysDicService.getValuesMap(varCode, pVarCodes);
-	}
+    @Override
+    public Map<String, Object> getValuesMap(String varCode, String... pVarCodes) {
+        return sysDicService.getValuesMap(varCode, pVarCodes);
+    }
 
-	@Override
-	public Map<String, Object> getSettings(String varCode) {
-		return sysDicService.getSettings(varCode);
-	}
+    @Override
+    public Map<String, Object> getSettings(String varCode) {
+        return sysDicService.getSettings(varCode);
+    }
 
-	@Override
-	public List<SysDicDTO> getChildren(Long id, Boolean recursion) {
-		return sysDicService.getChildren(id, recursion);
-	}
+    @Override
+    public List<SysDicDTO> getChildren(Long id, Boolean recursion) {
+        return sysDicService.getChildren(id, recursion);
+    }
 
-	@Override
-	public List<SysDicDTO> list(Long pid) {
-		return sysDicService.list(pid);
-	}
+    @Override
+    public List<SysDicDTO> list(Long pid) {
+        return sysDicService.list(pid);
+    }
 
-	@Override
-	public Boolean removeWithChildren(Long id) {
-		return sysDicService.removeWithChildren(id);
-	}
+    @Override
+    public Boolean removeWithChildren(Long id) {
+        return sysDicService.removeWithChildren(id);
+    }
 
-	@Override
-	public PageInfoVO<SysDicDTO> tops(Map<String, Object> params) {
-		return sysDicService.tops(params);
-	}
+    @Override
+    public PageInfoVO<SysDicDTO> tops(Map<String, Object> params) {
+        return sysDicService.tops(params);
+    }
+
+    @Override
+    public List<SysDicDTO> getChildren(String pVarCode, Boolean recursion) {
+        return sysDicService.getChildren(pVarCode, recursion);
+    }
 
 }
